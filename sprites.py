1fc6fda25e96cce6bf419c1657d5879492d86893
misories <sommie88@outlook.com>
2025-03-09 22:33:44 +0000

    1

--- sprites.py
+++ sprites.py
@@ -20,7 +20,7 @@ class Player(pygame.sprite.Sprite):
         self.load = pygame.image.load
         self.down1 = self.load("Pics/Sprite/slime2.png")
         self.image = pygame.Surface([self.w,self.h])
-        self.down1.pygame.transform.scale(self.down1, (16,16))
+        self.down1 = pygame.transform.scale(self.down1, (28,28))
         self.image.set_colorkey(BLACK)
         self.image.blit(self.down1,(0,0))
 
@@ -40,20 +40,31 @@ class Player(pygame.sprite.Sprite):
         self.right1 = self.load("Pics/Sprite/slime2right1.png")
         self.right2 = self.load("Pics/Sprite/slime2right2.png")
         self.right3 = self.load("Pics/Sprite/slime2right3.png")
+        self.right1 = pygame.transform.scale(self.right1, (28, 28))
+        self.right2 = pygame.transform.scale(self.right2, (28, 28))
+        self.right3 = pygame.transform.scale(self.right3, (28, 28))
 
         # Left Animation
         self.left1 = self.load("Pics/Sprite/slime2left1.png")
         self.left2 = self.load("Pics/Sprite/slime2left2.png")
         self.left3 = self.load("Pics/Sprite/slime2left3.png")
+        self.left1 = pygame.transform.scale(self.left1, (28, 28))
+        self.left2 = pygame.transform.scale(self.left2, (28, 28))
+        self.left3 = pygame.transform.scale(self.left3, (28, 28))
 
         # Up Animation
         self.up1 = self.load("Pics/Sprite/slime2up1.png")
         self.up2 = self.load("Pics/Sprite/slime2up2.png")
         self.up3 = self.load("Pics/Sprite/slime2up3.png")
+        self.up1 = pygame.transform.scale(self.up1, (28, 28))
+        self.up2 = pygame.transform.scale(self.up2, (28, 28))
+        self.up3 = pygame.transform.scale(self.up3, (28, 28))
 
         # Down Animation
         self.down2 = self.load("Pics/Sprite/slime2down2.png")
         self.down3 = self.load("Pics/Sprite/slime2down3.png")
+        self.down2 = pygame.transform.scale(self.down2, (28, 28))
+        self.down3 = pygame.transform.scale(self.down3, (28, 28))
 
     def update(self):
         self.movekeys()
@@ -173,7 +184,7 @@ class Walls(pygame.sprite.Sprite):
         self.rect.x = self.x
         self.rect.y = self.y
 
-class Grounds(pygame.sprite.Sprite):
+class Ground(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
 
         self.game = game
@@ -218,10 +229,10 @@ class NPC(pygame.sprite.Sprite):
         self.h = pixels
 
         self.load = pygame.image.load
-        self.down1 = self.load("Pics/Sprite/slime.png")
+        self.npc = self.load("Pics/Sprite/slime.png")
         self.image = pygame.Surface([self.w, self.h])
         self.image.set_colorkey(BLACK)
-        self.image.blit(self.down1, (0, 0))
+        self.image.blit(self.npc, (0, 0))
 
         self.rect = self.image.get_rect()
         self.rect.x = self.x
@@ -238,15 +249,16 @@ class Camera:
         self.x = start_x
         self.y = start_y
 
-    def update(self, target):
-        self.x = -target.rect.x + (self.w // 2)
-        self.y = -target.rect.y + (self.h // 2)
+    def update(self, player):
+        self.x = -player.rect.centerx + (self.w // 2)
+        self.y = -player.rect.centery + (self.h // 2)
 
-        self.x = max(-(self.w - 960), min(0, self.x))
-        self.y = max(-(self.h - 640), min(0, self.y))
-        self.camera = pygame.Rect(self.x, self.y, w, h)
+        self.x = max(-(world_w - 960), min(0, self.x))
+        self.y = max(-(world_h - 640), min(0, self.y))
+        self.camera = pygame.Rect(self.x, self.y, self.w, self.h)
 
     def apply(self, entity):
+        self.camera.topleft = (self.x, self.y)
         offset_x = -self.camera.topleft[0]
         offset_y = -self.camera.topleft[1]
         return entity.rect.move(-offset_x, -offset_y)
